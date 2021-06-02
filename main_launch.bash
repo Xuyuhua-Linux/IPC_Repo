079d6ff686cbf4e72d74b5a6de60210752cb2a56
Joshua Whitley <josh.whitley@autoware.org>
2021-06-02 12:32:53 -0400

    Update autoware service and fix FM1 folder

---
+++ main_launch.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
+export CYCLONEDDS_URI=file:///etc/cyclone/cyclonedds.xml
+export CYCLONE_INCLUDE=/opt/ros/foxy/include
+export CYCLONE_LIB=/opt/ros/foxy/lib/x86_64-linux-gnu
+export ROS_DOMAIN_ID=1
+export LD_LIBRARY_PATH=/usr/local/lib/
+source /home/user/adehome/AutowareAuto/install/setup.bash
+ros2 launch iac_launch vehicle.launch.py
