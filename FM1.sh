c6ab5a5dac6f55ae4a6bf36eb5826f46d2f708f1
Joshua Whitley <josh.whitley@autoware.org>
2021-07-19 15:26:21 -0400

    Zenoh --dds-allow options setup

--- FM1.sh
+++ FM1.sh
@@ -2,4 +2,4 @@
 sleep 3
 cd /home/user/zenoh-plugin-dds/target/release/
 ROS_DOMAIN_ID=1 RUST_LOG=debug ./zenoh-bridge-dds -d 1 -m peer -l tcp/10.42.0.200:7447 -s /DO12 -r /DO12/** -w /DO12/** \
---allow "ros_discovery_info|diagnostics|/joystick/.*|rt/lateral_error|lookahead_error|ct_report|misc_report_do|pt_report|/vehicle/.*|rc_to_ct|/raptor_dbw_interface/pt_report"
+--allow "ros_discovery_info|diagnostics|/joystick/.*|rt/lateral_error|lookahead_error|ct_report|misc_report_do|pt_report|/vehicle/.*|rc_to_ct|/raptor_dbw_interface/pt_report|/counter"
