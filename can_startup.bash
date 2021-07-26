04c9f228ede9a91af64f6b22ec40f64a67cb91df
Joshua Whitley <josh.whitley@autoware.org>
2021-07-26 09:04:35 -0400

    CAN instruction update, all 6 CAN interfaces now works

--- can_startup.bash
+++ can_startup.bash
@@ -1,4 +1,24 @@
+ip link set down can0
+ip link set down can1
+ip link set down can2
+ip link set down can3
+ip link set down can4
+ip link set down can5
 ip link set can0 type can bitrate 500000
 ip link set can1 type can bitrate 500000
+ip link set can2 type can bitrate 500000
+ip link set can3 type can bitrate 500000
+ip link set can4 type can bitrate 500000
+ip link set can5 type can bitrate 500000
+ifconfig can0 txqueuelen 1000
+ifconfig can1 txqueuelen 1000
+ifconfig can2 txqueuelen 1000
+ifconfig can3 txqueuelen 1000
+ifconfig can4 txqueuelen 1000
+ifconfig can5 txqueuelen 1000
 ip link set up can0
 ip link set up can1
+ip link set up can2
+ip link set up can3
+ip link set up can4
+ip link set up can5
