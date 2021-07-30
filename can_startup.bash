ce3fea4e1f77a622c1da6e8cb19ab2b93a901fd5
Joshua Whitley <josh.whitley@autoware.org>
2021-07-30 12:46:15 -0500

    Adding startup commands for all CAN channels

--- can_startup.bash
+++ can_startup.bash
@@ -1,4 +1,23 @@
 ip link set can0 type can bitrate 500000
-ip link set can1 type can bitrate 500000
+ip link set can0 txqueuelen 1000
 ip link set up can0
+
+ip link set can1 type can bitrate 500000
+ip link set can1 txqueuelen 1000
 ip link set up can1
+
+# ip link set can2 type can bitrate 500000
+# ip link set can2 txqueuelen 1000
+# ip link set up can2
+
+# ip link set can3 type can bitrate 500000
+# ip link set can3 txqueuelen 1000
+# ip link set up can3
+
+# ip link set can4 type can bitrate 500000
+# ip link set can4 txqueuelen 1000
+# ip link set up can4
+
+# ip link set can5 type can bitrate 500000
+# ip link set can5 txqueuelen 1000
+# ip link set up can5
